ed1796c9c93865cc7132be8eb96180ffef43013b
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-12-25 00:37:34 +0300

    feat: add springboot dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+
     kotlin("jvm") version "2.0.21"
+    kotlin("plugin.spring") version "2.0.21"
 }
 
 group = "org.url.shortener"
